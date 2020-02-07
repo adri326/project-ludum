419f70758544c64c0cafbea4455751878d863440
adrvillegas97 <adrian.villegas@eduvaud.ch>
2020-02-07 16:27:34 +0100

    LogIn-SignIn Interface

---
+++ NN/UserAccount/UserAccount/LogIn.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace UserAccount
+{
+    partial class LogIn
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 243);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 161);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Pseudo";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(417, 243);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(164, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(417, 161);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 307);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 81);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Log-in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 307);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 81);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Sing-up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 403);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 41);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Forgotten password ?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);            
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "LogIn";
+            this.Size = new System.Drawing.Size(833, 484);
+            this.Load += new System.EventHandler(this.LogIn_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ NN/unoM/unoM/Controllers/Controller1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using unoM.Models;
+
+namespace unoM.Controllers
+{
+    class Controller1
+    {
+        private View1 aView;
+        private Model1 aModel;
+        private ResourceManager imageManager;
+        private ResourceManager languageManger;
+
+        public View1 AView { get => aView; set => aView = value; }
+        internal Model1 AModel { get => aModel; set => aModel = value; }
+        public ResourceManager ImageManager { get => imageManager; set => imageManager = value; }
+        public ResourceManager LanguageManger { get => languageManger; set => languageManger = value; }
+
+        public Controller1(Model1 _aModel, View1 _aView)
+        {
+            imageManager = new ResourceManager(typeof(Resources.Images.images));
+            languageManger = new ResourceManager(typeof(Resources.i18n.en));
+
+            aModel = _aModel;
+            aView = _aView;
+
+            aView.AController = this;
+        }
+
+        public Image GetImage(string _id)
+        {
+            return (Image)imageManager.GetObject(_id);
+        }
+
+        public Icon GetIcon(string _id)
+        {
+            return (Icon)imageManager.GetObject(_id);
+        }
+
+        public Image ResizeImage(string _id, Size _size)
+        {
+            return (Image)(new Bitmap(GetImage(_id), _size));
+        }
+
+        public string GetText(string _id)
+        {
+            return languageManger.GetString(_id);
+        }
+
+        public void ChangeLanguage(string _langId)
+        {
+            switch (_langId)
+            {
+                case "frFlag":
+                    languageManger = new ResourceManager(typeof(Resources.i18n.fr));
+                    break;
+                case "enFlag":
+                default:
+                    languageManger = new ResourceManager(typeof(Resources.i18n.en));
+                    break;
+            }
+        }
+    }
+}
