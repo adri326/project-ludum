e99caceffa89bdc1f66575c51f6fd980bd0f77e1
dipaolath <dipaolath@etml.educanet2.ch>
2020-02-07 16:25:21 +0100

    ajout du menu avec les langues ajoutées(anglais,français) avec deux boutons pour changer de langue

--- Menu/Menu/Form1.Designer.cs
+++ Menu/Menu/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -35,13 +36,16 @@
             this.option = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.btnFrench = new System.Windows.Forms.Button();
+            this.btnEnglish = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(109)))), ((int)(((byte)(215)))));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(170)))), ((int)(((byte)(236)))));
             this.button1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 2;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
@@ -56,7 +60,8 @@
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(170)))), ((int)(((byte)(236)))));
             this.button2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 2;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
@@ -69,9 +74,10 @@
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(109)))), ((int)(((byte)(215)))));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(170)))), ((int)(((byte)(236)))));
             this.button3.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 2;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
@@ -86,7 +92,8 @@
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(170)))), ((int)(((byte)(236)))));
             this.button4.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderSize = 2;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.White;
@@ -99,9 +106,10 @@
             // 
             // option
             // 
-            this.option.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(109)))), ((int)(((byte)(215)))));
+            this.option.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(170)))), ((int)(((byte)(236)))));
             this.option.Cursor = System.Windows.Forms.Cursors.Default;
-            this.option.FlatAppearance.BorderSize = 0;
+            this.option.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.option.FlatAppearance.BorderSize = 2;
             this.option.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.option.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.option.ForeColor = System.Drawing.Color.White;
@@ -117,7 +125,8 @@
             // 
             this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(170)))), ((int)(((byte)(236)))));
             this.button6.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.BorderSize = 2;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.ForeColor = System.Drawing.Color.White;
@@ -130,9 +139,10 @@
             // 
             // button7
             // 
-            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(109)))), ((int)(((byte)(215)))));
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(170)))), ((int)(((byte)(236)))));
             this.button7.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.BorderSize = 2;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button7.ForeColor = System.Drawing.Color.White;
@@ -142,6 +152,45 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "Quitter";
             this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // btnFrench
+            // 
+            this.btnFrench.BackColor = System.Drawing.Color.Transparent;
+            this.btnFrench.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnFrench.BackgroundImage")));
+            this.btnFrench.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFrench.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnFrench.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnFrench.FlatAppearance.BorderSize = 0;
+            this.btnFrench.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnFrench.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnFrench.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFrench.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFrench.ForeColor = System.Drawing.Color.White;
+            this.btnFrench.Location = new System.Drawing.Point(1737, 47);
+            this.btnFrench.Name = "btnFrench";
+            this.btnFrench.Size = new System.Drawing.Size(106, 72);
+            this.btnFrench.TabIndex = 7;
+            this.btnFrench.UseVisualStyleBackColor = false;
+            this.btnFrench.Click += new System.EventHandler(this.btnFrench_Click);
+            // 
+            // btnEnglish
+            // 
+            this.btnEnglish.BackColor = System.Drawing.Color.Transparent;
+            this.btnEnglish.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEnglish.BackgroundImage")));
+            this.btnEnglish.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEnglish.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnEnglish.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnEnglish.FlatAppearance.BorderSize = 0;
+            this.btnEnglish.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnglish.ForeColor = System.Drawing.Color.White;
+            this.btnEnglish.Location = new System.Drawing.Point(1737, 136);
+            this.btnEnglish.Name = "btnEnglish";
+            this.btnEnglish.Size = new System.Drawing.Size(106, 69);
+            this.btnEnglish.TabIndex = 8;
+            this.btnEnglish.UseVisualStyleBackColor = false;
+            this.btnEnglish.Click += new System.EventHandler(this.btnEnglish_Click);
             // 
             // Form1
             // 
@@ -150,6 +199,8 @@
             this.BackgroundImage = global::Menu.Properties.Resources.uno;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1904, 1062);
+            this.Controls.Add(this.btnEnglish);
+            this.Controls.Add(this.btnFrench);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -174,6 +225,8 @@
         private System.Windows.Forms.Button option;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btnFrench;
+        private System.Windows.Forms.Button btnEnglish;
     }
 }
 
--- Menu/Menu/Form1.cs
+++ Menu/Menu/Form1.cs
@@ -1,26 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Menu.Resources;
+using System;
+using System.Resources;
 using System.Windows.Forms;
-
 namespace Menu
 {
     public partial class Form1 : Form
     {
+        //definis que la valeurs de base dur rm seront en FR//
+        ResourceManager rm = new ResourceManager(typeof(FR));
         public Form1()
         {
             InitializeComponent();
+            //affichage des textes selons la langue choisie
+            ChangeLanguage("FR");
+
+           
+           
+            
+        }
+       private void ChangeLanguage(string langue)
+        {
+            //change la langue //
+            switch (langue)
+            {
+                case "FR":
+                    rm = new ResourceManager(typeof(FR));
+                    break;
+                case "EN":
+                    rm = new ResourceManager(typeof(EN));
+                    break;
+                default:
+                    break;
+            }
+            button1.Text = rm.GetString("Multiplayer");
+            button2.Text = rm.GetString("SinglePlayer");
+            button3.Text = rm.GetString("GameHistory");
+            button4.Text = rm.GetString("Friends");
+            button6.Text = rm.GetString("Help");
+            button7.Text = rm.GetString("Exit");
         }
 
+        
         private void option_Click(object sender, EventArgs e)
         {
             Panel option = new Panel();
             
+
+        }
+
+        private void btnFrench_Click(object sender, EventArgs e)
+        {
+            ChangeLanguage("FR");
+        }
+
+        private void btnEnglish_Click(object sender, EventArgs e)
+        {
+           
+            ChangeLanguage("EN");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
